1d778482985bf07b1a847efd59119082a9387ba4
Guillermo Oramas <guillermo.oramas.1@gmail.com>
2019-08-22 09:49:32 -0500

    cambiando el correo de salida

--- VolunteersMiner/Mailers/UserMailer.cs
+++ VolunteersMiner/Mailers/UserMailer.cs
@@ -28,7 +28,7 @@ namespace VolunteersMiner.Mailers
             });
             var client = new SmtpClient("smtp.gmail.com", 587);
             client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("info@zip-cargo.com", "123Zipcargo+");
+            client.Credentials = new NetworkCredential("infomareaverde@gmail.com", "bob2019!");
             client.Send(mail);
         }
     }
