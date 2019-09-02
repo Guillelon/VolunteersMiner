e05573ea6912638933d42a0faf2a1fe76a8b9e51
Guillermo Oramas <guillermo.oramas.1@gmail.com>
2019-09-02 10:39:23 -0500

    Agregando valicaciones

--- DAL/Model/Poll.cs
+++ DAL/Model/Poll.cs
@@ -12,24 +12,33 @@ namespace DAL.Model
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         [Required]
+        [MaxLength(200)]
         public string FirstName { get; set; }
         [Required]
+        [MaxLength(200)]
         public string LastName { get; set; }
         [Required]
+        [MaxLength(200)]
         public string Email { get; set; }
+        [MaxLength(200)]
         public string Cellphone { get; set; }
         [Required]
         public string Age { get; set; }
         [Required]
         public string Province { get; set; }
         [Required]
+        [MaxLength(200)]
         public string Area { get; set; }
         public string TimeToParticipate { get; set; }
+        [MaxLength(200)]
         public string OtherTimeToParticipate { get; set; }
         [Required]
         public string TypeOfHelp { get; set; }
+        [MaxLength(200)]
         public string OtherTypeOfHelp { get; set; }
+        [MaxLength(200)]
         public string ProfessionInstitution { get; set; }
+        [MaxLength(200)]
         public string Profession { get; set; }
 
         public Poll()
